9ec47ee302a94aec9c1010d86292765d891e092a
venkateshpaila <venkateshpaila72@gmail.com>
2026-03-08 15:06:35 +0530

    last commit

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://billing-inventory-management-system.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
